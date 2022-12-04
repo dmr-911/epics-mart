60e8974ce5ceced06199f63ef7652ab32283b638
dmr-911 <dewan.mizanur911@gmail.com>
2022-12-04 16:08:11 +0600

    layout component created with basic layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Head from "next/head";
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div>
+      <div className="layout">
+        <Head>Epix</Head>
+        <header>
+          <Navbar/>
+        </header>
+        <main className="main-container">
+          {children}
+        </main>
+        <footer>
+          <Footer/>
+        </footer>
+      </div>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
