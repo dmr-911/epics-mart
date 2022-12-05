f38bffd707653b53e42b7de80262aed9f60cf32a
dmr-911 <dewan.mizanur911@gmail.com>
2022-12-05 12:16:33 +0600

    stripe implemented

--- components/Cart.js
+++ components/Cart.js
@@ -11,6 +11,7 @@ import { TiDeleteOutline } from "react-icons/ti";
 import { useStateContext } from "../context/StateContext";
 
 import { urlFor } from "../lib/client";
+import getStripe from "../lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
@@ -24,6 +25,28 @@ const Cart = () => {
     toggleCartItemQuantity,
     onRemove
   } = useStateContext();
+
+  const handleCheckout = async (e) =>{
+    e.preventDefault();
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method : 'POST',
+      headers : {
+        'Content-Type' : 'application/json'
+      },
+      body : JSON.stringify(cartItems)
+    });
+
+    if(response.status === 500) return;
+    const data = await response.json();
+
+    toast.loading('Redirectiong...');
+
+    stripe.redirectToCheckout({sessionId : data.id})
+
+  }
+
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
@@ -108,7 +131,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
+              <button type="button" className="btn" onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,34 +1,49 @@
-const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const params = {
         submit_type: "pay",
-        payment_method_types: ["cart"],
-        billing_address_collection: "auto",
-        shipping_options: [
-          {
-            shipping_rate : 'shr_1MBXAQE0hh98LK9HZ0s3B5UT',
-          },
-          {
-            shipping_rate : 'shr_1MBXBTE0hh98LK9Ha5FHGswO'
-          }
-        ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
         mode: "payment",
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [{ shipping_rate: "shr_1MBXAQE0hh98LK9HZ0s3B5UT" }],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn/sanity.io/images/txk0zvnb/production/"
+            )
+            .replace("-webp", ".webp");
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/`,
+        cancel_url: `${req.headers.origin}/canceled`,
       };
+
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
