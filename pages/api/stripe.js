7aa9093aa08261476f6f18f118cd824a55f8573c
dmr-911 <dewan.mizanur911@gmail.com>
2022-12-05 11:28:19 +0600

    stripe implemented on backend with stripe template

---
+++ pages/api/stripe.js
@@ -0,0 +1,39 @@
+const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const params = {
+        submit_type: "pay",
+        payment_method_types: ["cart"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          {
+            shipping_rate : 'shr_1MBXAQE0hh98LK9HZ0s3B5UT',
+          },
+          {
+            shipping_rate : 'shr_1MBXBTE0hh98LK9Ha5FHGswO'
+          }
+        ],
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: "{{PRICE_ID}}",
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
